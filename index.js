3cb17e219984bf6c9531cc5a320ac88d58423f79
Emerson Ramirez <emerson.ramirez.s@gmail.com>
2023-04-01 17:52:40 -0300

    actualizado con prepared statements

--- index.js
+++ index.js
@@ -13,27 +13,91 @@ const pool = new Pool({
 })
 
 
+//Primero se almacenan los parametros enviados desde la terminal en el arreglo args.
+let args;
+if (process.argv[2]){
+    args = process.argv.slice(2);
+    let opcion = args[0];
+    evaluarFuncion(opcion);
+}
 
-//SCRIPT PARA INSERTAR EN LA TABLA usuarios
-pool.query({
-    name: 'insertar',
-    text: 'INSERT INTO usuarios (nombre, apellido) VALUES ($1 , $2) RETURNING *',
-    values: ['Emerson','Ramazotti']
-}, (err,res)=>{
-    if (err) throw err
-    else {console.table(res.rows)}
-    pool.release;
-})
+//Luego se verifica el primer argumento enviado y se evalúa para determinar que función inicializar.
+function evaluarFuncion(opcion){
+    switch (opcion){
+        case 'insertar':
+            insertarUsuario();
+            break;
+        case 'consulta':
+            consultarUsuarios();
+            break;
+        case 'editar':
+            editarUsuario();
+            break;
+        case 'eliminar':
+            eliminarUsuario();
+            break;
+        default:
+            break;
+    }
+}
 
 
-//SCRIPT PARA MOSTRAR LA TABLA usuarios
-let arrayInfo;
-pool.query({
-    name: 'consulta',
-    text: 'SELECT * FROM usuarios',
-}, (err,res)=>{
-    if (err) throw err;
-    else {arrayInfo = res.rows}
-    pool.release;
-    console.log(arrayInfo);
-})
+//FUNCION PARA INSERTAR EN LA TABLA usuarios
+function insertarUsuario(){   
+    console.log(args);
+    pool.query({
+        name: 'insertar',
+        text: 'INSERT INTO usuarios (nombre, apellido) VALUES ($1 , $2) RETURNING *',
+        values: [args[1],args[2]]
+    }, (err,res)=>{
+        if (err) throw err
+        else {console.table(res.rows)}
+        pool.release;
+    })
+}
+
+//SCRIPT PARA MOSTRAR LA TABLA INDICADA
+function consultarUsuarios(){
+    let arrayInfo;
+    pool.query({
+        name: 'consulta',
+        text: 'SELECT * FROM usuarios'
+    }, (err,res)=>{
+        if (err) throw err;
+        else {arrayInfo = res.rows}
+        pool.release;
+        console.table(arrayInfo);
+    })
+}
+
+//FUNCION PARA EDITAR UN USUARIO POR ID
+function editarUsuario(){
+    pool.query({
+        name: 'editar',
+        text: `UPDATE usuarios SET nombre = $2 , apellido = $3 WHERE idusuario = $1 RETURNING *`,
+        values: [args[1],args[2],args[3]]      
+    },(err,res)=>{
+        if (err) throw err;
+        else {
+            console.log('Usuario editado con éxito.');
+            console.table(res.rows)
+        }
+        pool.release;
+    })
+}
+
+//FUNCION PARA ELIMINAR USUARIO POR ID
+function eliminarUsuario(){
+    pool.query({
+        name: 'eliminar',
+        text: 'DELETE FROM usuarios WHERE idusuario = $1 RETURNING *',
+        values: [args[1]]
+    },(err,res)=>{
+        if (err) throw err;
+        else {
+            console.log('Usuario eliminado con éxito.');
+            console.table(res.rows);
+        }
+        pool.release;
+    })
+}
