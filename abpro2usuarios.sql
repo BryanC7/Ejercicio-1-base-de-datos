96b4d4ae2ed97374ff7eefd1947bd9a71a9a6cd5
Bryan <bryancabreram3@gmail.com>
2023-04-03 09:11:51 -0400

    Cambios menores a lógica index

--- abpro2usuarios.sql
+++ abpro2usuarios.sql
@@ -1,6 +1,6 @@
--- Active: 1680180900307@@127.0.0.1@5432@abpro2@public
-CREATE TABLE usuarios(
+-- Active: 1680182167415@@127.0.0.1@5432@usuarios
+CREATE TABLE usuarios (
     idusuario SERIAL NOT NULL PRIMARY KEY,
     nombre VARCHAR(50),
     apellido VARCHAR(50)
-)
+);
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import pg from 'pg';
-const {Pool} = pg;
+import pg from 'pg'
+const {Pool} = pg
 
-const pool = new Pool({
+const pool = new Pool ({
     user: 'postgres',
     password: '1234',
     host: 'localhost',
     port: 5432,
-    database: 'abpro2',
+    database: 'usuarios', // Nombre de la base de datos utilizada actualmente
     max: 20, //numero de conexiones maximas
     idleTimeoutMillis: 5000, //cantidad maxima de milisegundos que puede estar inactiva la sesion antes de cerrarse
     connectionTimeoutMillis: 2000 //tiempo de espera para que se concrete la conexión, de lo contrario arroja error
@@ -14,59 +14,58 @@ const pool = new Pool({
 
 
 //Primero se almacenan los parametros enviados desde la terminal en el arreglo args.
-let args;
 if (process.argv[2]){
-    args = process.argv.slice(2);
-    let opcion = args[0];
-    evaluarFuncion(opcion);
+    evaluarFuncion(process.argv[2])
 }
 
 //Luego se verifica el primer argumento enviado y se evalúa para determinar que función inicializar.
 function evaluarFuncion(opcion){
     switch (opcion){
         case 'insertar':
-            insertarUsuario();
-            break;
+            insertarUsuario()
+            break
         case 'consulta':
-            consultarUsuarios();
-            break;
+            consultarUsuarios()
+            break
         case 'editar':
-            editarUsuario();
-            break;
+            editarUsuario()
+            break
         case 'eliminar':
-            eliminarUsuario();
-            break;
+            eliminarUsuario()
+            break
         default:
-            break;
+            break
     }
 }
 
 
 //FUNCION PARA INSERTAR EN LA TABLA usuarios
 function insertarUsuario(){   
-    console.log(args);
     pool.query({
         name: 'insertar',
         text: 'INSERT INTO usuarios (nombre, apellido) VALUES ($1 , $2) RETURNING *',
-        values: [args[1],args[2]]
-    }, (err,res)=>{
+        values: [process.argv[3], process.argv[4]]
+    }, (err,res) => {
         if (err) throw err
-        else {console.table(res.rows)}
-        pool.release;
+        else {
+            console.log('Usuario insertado con éxito.')
+            console.table(res.rows)
+        }
+        pool.release
     })
 }
 
 //SCRIPT PARA MOSTRAR LA TABLA INDICADA
 function consultarUsuarios(){
-    let arrayInfo;
     pool.query({
         name: 'consulta',
         text: 'SELECT * FROM usuarios'
-    }, (err,res)=>{
-        if (err) throw err;
-        else {arrayInfo = res.rows}
-        pool.release;
-        console.table(arrayInfo);
+    }, (err,res) => {
+        if (err) throw err
+        else {
+            console.table(res.rows)
+        }
+        pool.release
     })
 }
 
@@ -75,14 +74,14 @@ function editarUsuario(){
     pool.query({
         name: 'editar',
         text: `UPDATE usuarios SET nombre = $2 , apellido = $3 WHERE idusuario = $1 RETURNING *`,
-        values: [args[1],args[2],args[3]]      
+        values: [process.argv[3], process.argv[4], process.argv[5]]      
     },(err,res)=>{
-        if (err) throw err;
+        if (err) throw err
         else {
-            console.log('Usuario editado con éxito.');
+            console.log('Usuario editado con éxito.')
             console.table(res.rows)
         }
-        pool.release;
+        pool.release
     })
 }
 
@@ -91,13 +90,13 @@ function eliminarUsuario(){
     pool.query({
         name: 'eliminar',
         text: 'DELETE FROM usuarios WHERE idusuario = $1 RETURNING *',
-        values: [args[1]]
+        values: [process.argv[3]]
     },(err,res)=>{
-        if (err) throw err;
+        if (err) throw err
         else {
-            console.log('Usuario eliminado con éxito.');
-            console.table(res.rows);
+            console.log('Usuario eliminado con éxito.')
+            console.table(res.rows)
         }
-        pool.release;
+        pool.release
     })
 }
